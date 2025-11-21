e38ca93b207ad10830ba970993eff58cf1832c44
Rija Lalaina <rrazafindrabe@meilleurtaux.com>
2025-11-21 16:43:13 +0300

    Test for Panther Case Valid Form

--- tests/VideoPantherTest.php
+++ tests/VideoPantherTest.php
@@ -2,12 +2,16 @@
 
 namespace App\Tests;
 
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverExpectedCondition;
+use Facebook\WebDriver\WebDriverWait;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Panther\PantherTestCase;
 use Symfony\Component\DomCrawler\Field\FileFormField;
 
 class VideoPantherTest extends PantherTestCase
 {
-    public function testVideoPantherForm(): void
+    public function testVideoPantherValidForm(): void
     {
         // Lance Chrome (Panther WebDriver)
         $client = static::createPantherClient([
@@ -51,6 +55,38 @@ class VideoPantherTest extends PantherTestCase
 
         // Soumet le formulaire
         $client->submit($form);
+
+        $driver = $client->getWebDriver();
+
+        $wait = new WebDriverWait($driver, 10); // wait up to 10 seconds
+
+        // Vérifie que le poster de la vidéo est de la forme "/upload/thumbnails/69202d61abaa93.96087231.jpg"
+        $video = $wait->until(
+            WebDriverExpectedCondition::presenceOfElementLocated(
+                WebDriverBy::cssSelector('#videos_list video')
+            )
+        );
+
+        $poster = $video->getAttribute('poster');
+
+        $this->assertMatchesRegularExpression(
+            '/\/upload\/thumbnails\/[A-Za-z0-9]+\.[0-9]+\.jpg$/',
+            $poster
+        );
+
+        // Vérifie que la vidéo est de la forme "/upload/videos/69202d61acc1c6.83134536.mp4"
+        $source = $wait->until(
+            WebDriverExpectedCondition::presenceOfElementLocated(
+                WebDriverBy::cssSelector('#videos_list video source')
+            )
+        );
+
+        $src = $source->getAttribute('src');
+
+        $this->assertMatchesRegularExpression(
+            '/\/upload\/videos\/[A-Za-z0-9]+\.[0-9]+\.mp4$/',
+            $src
+        );
 
     }
 }
